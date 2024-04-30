eb99d7adfaa57a093ab07b8b7a61720e985bab8b
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-05-01 03:57:37 +0530

    Initial

---
+++ library/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Book
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'isbn', 'category']
+
+
+class BorrowForm(forms.Form):
+    book_id = forms.IntegerField(label='Book ID')
---
+++ library/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.models import User
+
+urlpatterns = [
+path('', views.user_login, name='user_login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('user/register/', views.user_register, name='user_register'),
+    path('user/login/', views.user_login, name='user_login'),
+    path('admin/login/', views.admin_login, name='admin_login'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('remove_book/<int:book_id>/', views.remove_book, name='remove_book'),
+    path('borrow/', views.borrow_book, name='borrow_book'),
+    path('my_books/', views.my_books, name='my_books'),
+]
